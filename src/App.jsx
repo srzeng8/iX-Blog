8867c4f14e9b346246b4bef5e69f3eafc44916e5
srzeng8 <srzeng@umass.edu>
2024-07-12 08:45:41 +0200

    fixed linking error

--- src/App.jsx
+++ src/App.jsx
@@ -2,26 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 import HomePage from "./components/HomePage";
 
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.min.js";
+import "bootstrap/dist/js/bootstrap.bundle.min";
+import "bootstrap-icons/font/bootstrap-icons.css";
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <div className="App">
-      <HomePage />
-      </div>
+    <HomePage />
     </div>
   );
 }
